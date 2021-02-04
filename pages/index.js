d68fea6b29da48f6bfdfb64e8e6ed85bb1cd45af
Yuri Thomas <yuri.tpinheiro@gmail.com>
2021-02-04 20:50:28 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
